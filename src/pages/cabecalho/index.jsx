111ada84d54b925fe54d1549f917886b806765f8
viktorortelan <jvortelan@gmail.com>
2024-07-24 15:38:46 -0300

    ok

---
+++ src/pages/cabecalho/index.jsx
@@ -0,0 +1,42 @@
+import "./cabe.scss";
+
+export default function Cabe() {
+    return (
+        <div>
+           <div className="mae">
+                <div className="cartao">
+                <img src="https://discord.com/assets/8bbdfca92495f8c3870b04fca1e98039.svg" alt="" />
+            <div className="cabe">
+            <div className="cabeImg">
+                <img src="https://media.discordapp.net/attachments/837420691666108437/1265041559775350825/logo-discord-removebg-preview.png?ex=66a01143&is=669ebfc3&hm=5217e5d9ee1a427ccb33d6d22f45cdea876b3637a4bfc30c33e37d1602edd9e7&=&format=webp&quality=lossless" alt="" />
+            </div>
+
+            <div className="infoCabe">
+                <a href="/">Download</a>
+                <a href="/">Nitro</a>
+                <a href="/">Discover</a>
+                <a href="/">Safety</a>
+                <a href="/">Support</a>
+                <a href="/">Blog</a>
+                <a href="/">Careers</a>
+            </div>
+
+            <div className="cabLogin">
+                <a href="/">Login</a>
+            </div>
+            
+            </div>
+
+            <div className="seccao1">
+                <h1>FIND YOUR COMMUNITY ON DISCORD</h1>
+                <p>From gaming, to music, to learning, there's a place for you.</p>
+            </div>
+
+                </div>
+                <img className="esquerda" src="https://media.discordapp.net/attachments/837420691666108437/1265053800218230844/esquerda-removebg-preview.png?ex=66a01ca9&is=669ecb29&hm=f2643e38924562dfbee28daef4efcf72c79e573fd355dfd28afac2fa778246ef&=&format=webp&quality=lossless" alt="" />
+                <img className="direita" src="https://media.discordapp.net/attachments/837420691666108437/1265053799878365325/direita-removebg-preview.png?ex=66a01ca9&is=669ecb29&hm=c7da690dd9f2f4e4e194b0836068354d4dd6732c41b355b9e97ce19cc5bc39c9&=&format=webp&quality=lossless" alt="" />
+           </div>
+                
+        </div>
+    )
+}
